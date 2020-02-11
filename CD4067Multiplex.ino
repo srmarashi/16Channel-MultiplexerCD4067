399dcc4eb7c60f69cc3b2ceac79630f29582fc67
seyed roohollah marashi <srmarashi@yahoo.com>
2020-02-11 12:44:07 +0330

    CD4067 16 Channel Analog Multiplexer
    Connect CD4067 to Arduino and Extend more 16 Channel analog input even!! ouput

---
+++ CD4067Multiplex.ino
@@ -0,0 +1,64 @@
+//8 channels cd4067 Library Free changes by: S.R.Marashi   srmarashi@yahoo.com
+// Here is pinout for HW-529 CD4051 MultiPlexer Digital To analog
+// S0 --> Arduino Digital 3
+// S1 --> Arduino Digital 4
+// S2 --> Arduino Digital 5
+// S3 --> Arduino Digital 6
+// E --> Arduino Digital 2
+// Z --> Arduino analog A0
+// VEE/GND --> Arduino GND (Connect Both of them to GND)
+// VCC --> Arduino +5V
+// Y0 to Y7 --> Analog Extended Pins
+
+const byte ENPIN = 2; //Enable pin
+const byte APIN = 3; //S0
+const byte BPIN = 4; //S1
+const byte CPIN = 5; //S2
+const byte DPIN = 6; //S3
+
+
+// an analog pin need to Cycle Digital Pins
+const byte SPIN = A0;
+
+void setup() {
+
+  //  pins out and LOW
+  pinMode(APIN, OUTPUT);
+  pinMode(BPIN, OUTPUT);
+  pinMode(CPIN, OUTPUT);
+  pinMode(DPIN, OUTPUT);
+  digitalWrite(APIN, LOW);
+  digitalWrite(BPIN, LOW);
+  digitalWrite(CPIN, LOW);
+  digitalWrite(DPIN, LOW);
+
+  // Enable pin should have Voltage for activating module
+  pinMode(ENPIN, OUTPUT);
+  digitalWrite(ENPIN, LOW);
+
+  Serial.begin(9600);
+  Serial.println("1 analog and 3 Digital pins give 16 ANALOG Low precision PIN ");
+}
+
+void loop()
+{
+
+  // for example pin0 and pin7 will read continusely and will print out analog Result Valuue
+  Serial.println(CycleRecall(15));
+   Serial.println(CycleRecall(9));
+  Serial.println(CycleRecall(0));
+  delay(100);
+}
+
+
+// reading function for the CD4051B
+int CycleRecall(byte channel) {
+
+  digitalWrite(APIN, bitRead(channel, 0));
+  digitalWrite(BPIN, bitRead(channel, 1));
+  digitalWrite(CPIN, bitRead(channel, 2));
+  digitalWrite(DPIN, bitRead(channel, 3));
+  
+  //Cyle ReCall
+  return analogRead(SPIN);
+}
